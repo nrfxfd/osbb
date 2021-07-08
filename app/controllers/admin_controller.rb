6ba6e84804f2070464046114a371b9fc9e4db76e
antk <nrfxfd@gmail.com>
2021-07-09 00:44:43 +0300

    Ver 0.1

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,53 @@
+class AdminController < ApplicationController
+
+    #   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+    def index
+      @appartments = appartment.all
+    end
+
+    def show
+      @appartments = appartment.find(params[:id])
+    end
+
+    def new
+      @appartments = appartment.new
+    end
+
+    def create
+      @appartments = appartment.new(article_params)
+
+      if @appartment.save
+        redirect_to @appartment
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @appartments = appartment.find(params[:id])
+    end
+
+    def update
+      @appartments = appartment.find(params[:id])
+
+      if @appartment.update(article_params)
+        redirect_to @appartment
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @appartments = appartment.find(params[:id])
+      @appartment.destroy
+
+      redirect_to @appartment_path
+    end
+
+    private
+    def appartment_params
+      params.require(:article).permit(:title, :body, :status)
+    end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'residents/index'
+  get 'appartments/index'
   root "home#index", as: 'home'
 
   get "/video", to: "video#index"
   get "/login", to: "login#index"
 
   resources :documents
-
-
+  resources :appartments
   resources :articles do
     resources :comments
   end
- 
+
 end
