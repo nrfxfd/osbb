ba8446ef97ba28f77a96e6d0ce99817b6d1ac0a7
antk <nrfxfd@gmail.com>
2021-07-18 18:05:47 +0300

    Merge branch 'develop'

--- app/models/appartment.rb
+++
@@ -1,2 +0,0 @@
-class Appartment < ApplicationRecord
-end
--- test/models/appartment_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AppartmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
