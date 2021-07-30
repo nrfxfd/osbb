57afc73f05c436e63942a754ae43f3de43b482cc
antk <nrfxfd@gmail.com>
2021-07-30 08:29:22 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,32 @@
 class LoginController < ApplicationController
   def index
-    @login = 'Autorization, Read, Update counters, Payment'
+    @apartments = Apartment.all
   end
+
+  def show
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def edit
+    @apartment = Apartment.find(params[:id])
+  end
+
+  def update
+    @apartment = Apartment.find(params[:id])
+
+    if @apartment.update(apartment_params )
+      redirect_to apartment_path @apartment
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit( :apartment_number, :apartment_area,
+                                       :heating_counter,
+                                       :water_counter,:electricity_counter,:arrears)
+  end
+
 end
